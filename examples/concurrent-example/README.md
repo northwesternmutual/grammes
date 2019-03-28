f5a1504f024a93b77a6cbf5d454fef27a88e6a41
Damien Stamates <damienstamates@northwesternmutual.com>
2019-03-28 12:36:32 -0500

    Added concurrency example for adding vertices

---
+++ examples/concurrent-example/README.md
@@ -0,0 +1,54 @@
+# concurrent-example
+
+Shows off that the grammes client can be used concurrently in goroutines.
+
+## Description
+
+**concurrent-example** shows how the grammes client is able to be used concurrently in goroutines. It will add 25 vertices and then wait until all goroutines are finished. After the test is done you can examine the vertices in the Gremlin console if curious.
+
+## Prerequisites
+
+- Go 1.11.1
+- Git
+- Elastic Search
+- Cassandra
+  - Java 8
+
+## Running
+
+To run this test you will need a TinkerPop server running and a graph database to connect to locally. This example was tested while using JanusGraph which can be used by locating yourself to the root directory of the Grammes project.
+
+``` sh
+cd $GOPATH/src/github.com/northwesternmutual/grammes
+```
+
+After locating yourself here then you may change directory to the `/scripts` folder.
+
+``` sh
+cd scripts
+```
+
+Finally you may run the `janusgraph.sh` script to begin a local instance of JanusGraph. This will begin the TinkerPop server for you as well.
+
+``` sh
+./janusgraph.sh
+```
+
+For further instructions please find yourself to the root [README.md](../../README.md) file.
+
+## Steps
+
+### General steps
+
+- Create a [zap](https://github.com/uber-go/zap) logger to help explain what's going on in the test and display the results.
+- Creates a Grammes client that connects to a locally hosted [TinkerPop](http://tinkerpop.apache.org/) server with a WebSocket.
+  - For testing this was created using JanusGraph. This can be run in the `/scripts` directory.
+- Drop all of the possible interfering vertices that were already on the graph.
+- Defer a drop of all the testing vertices. This is done as clean up.
+
+---
+
+### Test specific steps
+
+- Adds testing vertices to the graph concurrently
+- Logs the vertex count
---
+++ examples/concurrent-example/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"sync"
+
+	"github.com/northwesternmutual/grammes"
+	"github.com/northwesternmutual/grammes/examples/exampleutil"
+	"go.uber.org/zap"
+)
+
+var (
+	// addr is used for holding the connection IP address.
+	// for example this could be, "ws://127.0.0.1:8182"
+	addr string
+)
+
+func main() {
+	flag.StringVar(&addr, "h", "", "Connection IP")
+	flag.Parse()
+
+	logger := exampleutil.SetupLogger()
+	defer logger.Sync()
+
+	if addr == "" {
+		logger.Fatal("No host address provided. Please run: go run main.go -h <host address>")
+		return
+	}
+
+	// Create a new Grammes client with a standard websocket.
+	client, err := grammes.DialWithWebSocket(addr)
+	if err != nil {
+		logger.Fatal("Couldn't create client", zap.Error(err))
+	}
+
+	// DropAll will remove all vertices from the graph currently.
+	// Essentially blank slating all of our data.
+	client.DropAll()
+
+	// Create a waitgroup to wait until all go routines are finished.
+	var wg sync.WaitGroup
+
+	// Add in 25 vertices to the graph.
+	for i := 0; i < 25; i++ {
+		// Add one new task to the waitgroup.
+		wg.Add(1)
+
+		// Launch the go routine.
+		go func(cli *grammes.Client, index int, logger *zap.Logger, wg *sync.WaitGroup) {
+			// Add a vertex to the graph concurrently.
+			v, err := cli.AddVertex(fmt.Sprintf("vert%d", index))
+			if err != nil {
+				logger.Fatal("Couldn't create vertex", zap.Error(err))
+			}
+			// Add a property to the newly created vertex.
+			v.AddProperty(cli, "name", "Damien")
+			// Send a message that the waitgroup is finished.
+			wg.Done()
+		}(client, i, logger, &wg)
+	}
+
+	// Wait until all of the goroutines are finished.
+	wg.Wait()
+
+	// Log that the goroutines are done.
+	logger.Info("Done.")
+
+	// Count the vertices on the graph
+	count, err := client.VertexCount()
+	if err != nil {
+		logger.Fatal("Couldn't count vertices", zap.Error(err))
+	}
+
+	// Log the count of the vertices.
+	logger.Info("Counted vertices", zap.Int64("count", count))
+}
