fa84de086c72596a31d4fd28471d8a814acf6a9a
UdiRaitblat <udi@beyond.security>
2021-02-17 18:11:48 +0200

    WZ-2623 clean

--- request.go
+++ request.go
@@ -33,10 +33,23 @@ var (
 	gremPrepareAuthRequest = gremconnect.PrepareAuthRequest
 )
 
+const (
+	defaultServerTimeout = 120 * time.Second
+	clientTimeoutGrace = 10 * time.Second
+)
+
 func (c *Client) executeRequest(query string, queryTimeout *time.Duration, bindings, rebindings map[string]string) ([][]byte, error) {
-	resolvedQueryTimeout := c.requestTimeout
+	clientTimeout := c.requestTimeout
+	var serverTimeout *time.Duration
+
 	if queryTimeout != nil {
-		resolvedQueryTimeout = *queryTimeout
+		clientTimeout = *queryTimeout
+	}
+
+	if clientTimeout > defaultServerTimeout {
+		clientTimeoutCopy := clientTimeout
+		serverTimeout = &clientTimeoutCopy
+		clientTimeout = clientTimeout + clientTimeoutGrace
 	}
 
 	err := c.requestSemaphore.Acquire(context.Background(), 1)
@@ -48,13 +61,9 @@ func (c *Client) executeRequest(query string, queryTimeout *time.Duration, bindi
 	}
 	defer c.requestSemaphore.Release(1)
 
-	var gremRequestTimeout *time.Duration
-	// if resolvedQueryTimeout > 120 *time.Second { // Don't set lower than default value
-		gremRequestTimeout = &resolvedQueryTimeout
-	// }
 	// Construct a map containing the values along
 	// with a randomly generated id to fetch the response.
-	req, id, err := gremPrepareRequest(query, gremRequestTimeout, bindings, rebindings)
+	req, id, err := gremPrepareRequest(query, serverTimeout, bindings, rebindings)
 	if err != nil {
 		c.logger.Error("uuid generation when preparing request",
 			gremerror.NewGrammesError("executeRequest", err),
@@ -73,7 +82,7 @@ func (c *Client) executeRequest(query string, queryTimeout *time.Duration, bindi
 
 	c.resultMessenger.Store(id, make(chan int, 1))
 	c.dispatchRequest(msg)              // send the request.
-	resp, err := c.retrieveResponse(id, resolvedQueryTimeout+20*time.Second) // retrieve the response from the gremlin server
+	resp, err := c.retrieveResponse(id, clientTimeout) // retrieve the response from the gremlin server
 	if err != nil {
 		c.logger.Error("retrieving response",
 			gremerror.NewGrammesError("executeRequest", err),
